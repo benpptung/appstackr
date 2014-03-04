2d5057bc976ae27ca28fe94018ccc732049dadf1
benpptung <benpptung@tacol.biz>
2014-03-04 19:11:40 +0800

    A weird bug, the hash of webmake result keeps changing

--- test/utils.js
+++ test/utils.js
@@ -17,8 +17,8 @@ var profile = require('./support'),
     async = require('async'),
     prettyjson = require('prettyjson');
 
-var webmake_file_hash = 'a05531a72ccd74e28817dfed1d0390a7c721b036',
-    webmake_files_hash = '8c7d26626e92c722daa4c87bac432221d68888be',
+var webmake_file_hash = 'd091813a3a27e39032e9b7611903d0d293b9c1b3',
+    webmake_files_hash = 'a76a9ac35ac8a612a2c723e959553500642319ca',
     browserify_file_hash = '5e372ed784133892007ff3bb7240b8f34b39a497',
     browserify_files_hash = '7137e1ea4841dd91c3f7be7f2bf3e03cba744ac6',
     concat_files_hash = '5513854f28f4c426832b3832acb42bc4006d527a',
